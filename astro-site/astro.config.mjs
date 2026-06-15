9ad623ee3fafc4e63fa0fde5cafb2e04087ef327
João Lázaro Brito Dias <63989543+LarsLouch@users.noreply.github.com>
2026-06-14 21:33:49 -0300

    Add files via upload

--- astro-site/astro.config.mjs
+++ astro-site/astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
-  // Optimize all images automatically
-  image: {
-    domains: [],
-    service: {
-      entrypoint: '@astrojs/image/sharp',
-    },
-  },
-  // Disable SSR since this is a static site
-  ssr: false,
+ integrations: [],
+ // Optimize all images automatically
+ image: {
+ domains: [],
+ service: {
+ entrypoint: '@astrojs/image/sharp',
+ },
+ },
+ // Enable SSR for dynamic content and authentication
+ ssr: true,
 });
