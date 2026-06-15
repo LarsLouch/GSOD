61a2c07874b14f4202e9d8214fae79ff9c6c989c
João Lázaro Brito Dias <63989543+LarsLouch@users.noreply.github.com>
2026-06-15 05:28:54 -0300

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import { vercel } from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
- // Enable SSR for dynamic content and authentication
- ssr: true,
+  // Enable SSR for dynamic content and authentication
+  ssr: true,
+  adapter: vercel(),
 });
